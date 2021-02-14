a9ad70933807d48ea3ef1ea47916f6b43fb7ff47
Luan <luanfreitas12@hotmail.com>
2021-02-14 00:49:25 -0300

    Unform

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { Container, Content, Background } from './styles';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    // eslint-disable-next-line no-console
+    console.log(data);
+  }
+
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu Cadastro</h1>
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            placeholder="Senha"
+            type="password"
+          />
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+
+        <a href="Login">
+          <FiArrowLeft />
+          Voltar para Logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
